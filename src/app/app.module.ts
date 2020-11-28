5473e85e7ab226e9d347d2366e8fd9defbf33282
elyramirez <eramirez@redmagisterial.com>
2020-11-27 22:19:56 -0600

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './componen/home/about-us/about-us.component';
+import { WhatWeDoForYouComponent } from './component/home/what-we-do-for-you/what-we-do-for-you.component';
+import { ToolsComponent } from './component/home/tools/tools.component';
+import { DishesComponent } from './component/home/dishes/dishes.component';
+import { CustomersComponent } from './component/home/customers/customers.component';
+import { ChefComponent } from './component/home/chef/chef.component';
+import { MomentsComponent } from './component/home/moments/moments.component';
+import { GalleryComponent } from './component/home/gallery/gallery.component';
+import { TestifyComponent } from './component/home/testify/testify.component';
+import { AcademyComponent } from './component/home/academy/academy.component';
+import { BlogComponent } from './component/home/blog/blog.component';
+import { ContactComponent } from './component/home/contact/contact.component';
+import { FooterComponent } from './component/home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    WhatWeDoForYouComponent,
+    ToolsComponent,
+    DishesComponent,
+    CustomersComponent,
+    ChefComponent,
+    MomentsComponent,
+    GalleryComponent,
+    TestifyComponent,
+    AcademyComponent,
+    BlogComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/home/what-we-do-for-you/what-we-do-for-you.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-what-we-do-for-you',
+  templateUrl: './what-we-do-for-you.component.html',
+  styleUrls: ['./what-we-do-for-you.component.css']
+})
+export class WhatWeDoForYouComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
