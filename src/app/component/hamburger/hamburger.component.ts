c1faaf4962dd8dcb1a79b8c94f376d07516684fd
maag070208 <asael070208@gmail.com>
2020-11-28 10:38:59 -0800

    disable and enable components

--- src/app/component/hamburger/hamburger.component.ts
+++ src/app/component/hamburger/hamburger.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, } from '@angular/core';
 
 @Component({
   selector: 'app-hamburger',
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hamburger.component.css']
 })
 export class HamburgerComponent implements OnInit {
-
+ 
   constructor() { }
-
+ 
   ngOnInit(): void {
   }
- 
+
+  visible: String = 'false';
+  
+  visibleClick = () =>{
+     //Navbar Items
+      let element = document.getElementById('hamButtom');
+      let Nosotros = document.getElementById('nosotros-1');
+      let Herramientas = document.getElementById('herramientas-1');
+      let Platos = document.getElementById('platos-digitales-1');
+    
+      //Components
+      let Start = document.getElementById('start');
+      let About = document.getElementById('about-us');
+      
+      //Navbar Items
+      element.className = 'extra-menu';
+      Nosotros.classList.replace('disableHamItems', 'item1');
+      Herramientas.classList.replace('disableHamItems', 'item2');
+      Platos.classList.replace('disableHamItems', 'item3');
+      
+      //Components
+      Start.className = 'disableComponents';
+      About.className = 'disableComponents';
+  }
+
+  invisibleClick = () =>{
+     //Navbar Items
+      let element = document.getElementById('hamButtom');
+      let Nosotros = document.getElementById('nosotros-1');
+      let Herramientas = document.getElementById('herramientas-1');
+      let Platos = document.getElementById('platos-digitales-1');
+
+      //Components
+      let Start = document.getElementById('start');
+      let About = document.getElementById('about-us');
+
+      //Navbar Items
+      element.className = 'container-hamburger effect';
+      Nosotros.className = 'disableHamItems';
+      Herramientas.className ='disableHamItems';
+      Platos.className ='disableHamItems';
+      
+      //Components
+      Start.classList.replace('disableComponents', 'start');
+      About.classList.replace('disableComponents', 'about');
+  }
 }
